8f09b483ad738aef292071b189bafcb70f272242
Evan Garcia <evanag116@yahoo.com>
2022-06-05 18:48:43 -0500

    completed in js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,13 @@
 exports.palindrome = function(word) {
+    let reverse = ""
+    let testWord = word.toString().toLowerCase().replace(" ", "")
+
+
+    for (let i = testWord.length-1; i > -1; i--) {
+        reverse += testWord[i]
+    }
+    return reverse == testWord
 
 };
+
+console.log(this.palindrome("racecar"))
